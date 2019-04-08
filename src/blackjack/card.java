450e7aebe4062df550cec5d81f5c92b7359ec13c
Richard Jones <richardj99@sky.com>
2019-04-08 11:59:11 +0100

    First Bracnh commit
    Publishing first branch deviation

--- src/blackjack/card.java
+++ src/blackjack/card.java
@@ -10,9 +10,21 @@ package blackjack;
  * @author Rory
  */
 public class card {
-    private int priority;
+    private short priority;  // Shows suit and card priority
     private int value;
     private char suit;
+    private String title;
+    
+    public card(int inputTitle, char suitInput) {
+    	title = Integer.toString(inputTitle);
+    	value = inputTitle;
+    }
+    
+    public card(String inputTitle, char suitInput) {
+    	title = inputTitle;
+    	if(inputTitle.equals("A")) value = 11;
+    	else value = 10;    	
+    }
     
     // getters
     public int getPriority(){
--- src/blackjack/deck.java
+++ src/blackjack/deck.java
@@ -7,21 +7,28 @@ package blackjack;
 
 /**
  *
- * @author Rory
+ * @author SLAD Team 3, Rory Gee, Shafqat 
  */
 public class deck {
     private int deckNo;
     //private x cards; use array of cards
+    
+    // constructor method
+    public deck(int chosenDeckNo){
+    	deckNo = chosenDeckNo;
+    }// end of constructor method
+    
     
     // getters
     public int getDeckNo(){
         return deckNo;
     }
     
+    // TODO: Discuss whether this setter is necessary (deck number is decided on construction).
     // setters
-    public void setDeckNo(int newDeckNo){
+    /*public void setDeckNo(int newDeckNo){
         this.deckNo = newDeckNo;
-    }
+    } */
     
     // methods
     public void shoe(){
